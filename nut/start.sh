fda7397ee16b69b882ad76c50c77f8a7a2ceea0e
Russ Sirois <rsirois+github@pm.me>
2025-07-23 21:37:40 -0700

    testing

--- nut/start.sh
+++ nut/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-rm -rf /var/run/nut/* 2> /dev/null && mkdir -m 700 /var/run/nut && chown nut:nut /var/run/nut
+rm -rf /var/run/nut/* 2> /dev/null && mkdir -m 700 /var/run/nut 2> /dev/null && chown nut:nut /var/run/nut
 upsdrvctl -FF start &
 upsd -FF
-exit 1
+echo "Fell through"
+upsc -l
+exit 99
