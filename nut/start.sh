a8740782d78f0f77ec8c3e0b9889515e37720a80
Russ Sirois <rsirois+github@pm.me>
2025-07-24 09:11:58 -0700

    improvements

--- nut/start.sh
+++ nut/start.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
+# Housekeeping
 rm -rf /var/run/nut/* 2> /dev/null && mkdir -m 700 /var/run/nut 2> /dev/null && chown nut:nut /var/run/nut
+chown -R nut:nut /etc/nut && chmod -R 700 /etc/nut
+
+# Start
 upsdrvctl -FF start &
 upsd -FF
-echo "Fell through"
+
+# Programs exited
+echo "Fell through, dumping state and quitting"
 upsc -l
-exit 99
+exit 1
