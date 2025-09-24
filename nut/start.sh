7b2a08e3059574ec959ecee28e114d707db2883b
Russ Sirois <rsirois+github@pm.me>
2025-09-24 14:40:44 -0700

    minor changes

--- nut/start.sh
+++ nut/start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # Housekeeping
-rm -rf /var/run/nut/* 2> /dev/null && mkdir -m 700 /var/run/nut 2> /dev/null && chown nut:nut /var/run/nut
-chown -R nut:nut /etc/nut && chmod -R 700 /etc/nut
+rm -rf /var/run/nut/* 2> /dev/null
+mkdir /var/run/nut 2> /dev/null
+chown -R nut:nut /etc/nut /var/run/nut
+chmod -R 700 /etc/nut /var/run/nut
 
 # Start
 upsdrvctl -FF start &
